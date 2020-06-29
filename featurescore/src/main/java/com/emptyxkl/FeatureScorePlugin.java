9b7a5c6f86363fda26874f647b6f476e23dd3b04
ZackMa <chattmazio@gmail.com>
2020-06-29 13:12:19 +0800

    sdfa

--- featurescore/src/main/java/com/emptyxkl/FeatureScorePlugin.java
+++ featurescore/src/main/java/com/emptyxkl/FeatureScorePlugin.java
@@ -3,6 +3,7 @@ package com.emptyxkl;
 import org.elasticsearch.plugins.ActionPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.ScriptPlugin;
+import org.elasticsearch.script.ScriptContext;
 
 import java.util.Collections;
 import java.util.List;
@@ -10,11 +11,10 @@ import java.util.List;
 public class FeatureScorePlugin extends Plugin implements ActionPlugin, ScriptPlugin {
 
     public FeatureScorePlugin(){
-        super();
     }
 
     @Override()
-    public List getContexts(){
-        return Collections.singletonList(new FeatureScorePluginHandle());
+    public List<ScriptContext<?>> getContexts(){
+        return Collections.<ScriptContext<?>>singletonList(new FeatureScorePluginHandle());
     }
 }
